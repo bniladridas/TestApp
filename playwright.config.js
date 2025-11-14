453b11ea1b0f74991b5f1e23c4f6a4e8b4139d19
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-14 12:59:10 +0530

    fix: update playwright config for...

--- playwright.config.js
+++ playwright.config.js
@@ -66,7 +66,7 @@ export default defineConfig({
   webServer: {
     command: process.env.CI
       ? 'npx vite --host 127.0.0.1 --port 5173'
-      : 'npm run dev',
+      : 'vite --host 127.0.0.1 --port 5173',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
